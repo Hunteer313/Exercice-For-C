399e1ae01c0b2b8393face1b6117bdea7a99bff3
Hunteer313 <bih.zakaria1@gmail.com>
2025-05-04 00:45:30 +0100

    first commit

---
+++ BasicDeclarations&Expressions/Exercice1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Name\t: Alexandra Abramov\n");
+    printf("DOB\t: uly 14, 1980\n");
+    printf("Mobile\t: 0808070605\n");
+    
+    return 0;
+
+}
