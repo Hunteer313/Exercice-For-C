43d658e9cc891949cea9359735d1f4e7e8654eee
Hunteer313 <bih.zakaria1@gmail.com>
2025-05-05 23:45:14 +0100

    Exercice

--- BasicDeclarations&Expressions/Exercice1.c
+++ BasicDeclarations&Expressions/Exercice1.c
@@ -1,3 +1,12 @@
+/*Write a C program to print your name, date of birth, and mobile number.
+
+Expected Output:
+
+Name   : Alexandra Abramov  
+DOB    : July 14, 1975  
+Mobile : 0808070605*/
+
+
 #include <stdio.h>
 #include <stdlib.h>
 
